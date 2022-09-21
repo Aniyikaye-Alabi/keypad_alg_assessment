3bae5973521af812e6031fae03e6d5e08d30a6a2
Aniyikaye Alabi <adalabi@sidmach.com>
2022-09-21 23:56:06 +0100

    Code refractor and formatting

--- david_keypad_alg.js
+++ david_keypad_alg.js
@@ -14,11 +14,12 @@
         ];
         
         
-        let realPhoneNo = "";
-        let phone_no = [];
-        let storeNum;
-        for(x = 0; x < 7; x++){
-        
+    let realPhoneNo = "";
+    let phone_no = [];
+    let storeNum;
+
+    for(x = 0; x < 7; x++){
+    
         // Start number
         let startNum;
 
@@ -31,32 +32,34 @@
           startNum = storeNum;
         }
         
+        // loop into the keypads
         for(i = 0; i < keypad.length; i++){
           for(j=0; j < keypad[i].length; j++){
               if(keypad[i][j] == startNum){
-                var a = keypad[i].concat(keypad.map(e => e[keypad[i].indexOf(keypad[i][j])]));
+                var a = keypad[i].concat(keypad.map(e => e[keypad[i].indexOf(keypad[i][j])])); //pick the rows and columns of the seleceted startnum based on the rook piece.
                 validnum = a;
               }
             }
-         
+        
         }
         
-        let filteredarray = validnum.filter(x => x != startNum && x != '*' && x != '#');
-        let randomNum = Math.floor(Math.random() * filteredarray.length);
+        let filteredarray = validnum.filter(x => x != startNum && x != '*' && x != '#'); // Don't pick the the original startnum, *, #.
+        let randomIndexNum = Math.floor(Math.random() * filteredarray.length);
         
         phone_no.push(startNum);
-        storeNum = filteredarray[randomNum];
-        
-        }
-        phone_no.forEach(element => {
-          realPhoneNo += element;
-        });
-        var realPhoneNoFormat = realPhoneNo.slice(0, 3) + "-" + realPhoneNo.slice(3);
+        storeNum = filteredarray[randomIndexNum]; // select next number from the filtered valid number lists.
+    
+    }
 
-        if(realPhoneNoFormat[0] != '0' && realPhoneNoFormat[0] != '1'){
-          console.log(realPhoneNoFormat);
-        }
-        else{
-          console.log("Invalid number is generated, number cannot start with 0 or 1");
-        }
+    phone_no.forEach(element => {
+      realPhoneNo += element; // populate realPhoneNo with all the phone numbers.
+    });
+    var realPhoneNoFormat = realPhoneNo.slice(0, 3) + "-" + realPhoneNo.slice(3);
+
+    if(realPhoneNoFormat[0] != '0' && realPhoneNoFormat[0] != '1'){
+      console.log(realPhoneNoFormat);
+    }
+    else{
+      console.log("Invalid number is generated, number cannot start with 0 or 1");
+    }
         
